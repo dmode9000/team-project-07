561cb40afc113d9fbe9e7f3de60ee72ba76cd88d
Dmode <dmode90@gmail.com>
2025-03-22 02:44:27 +0200

    mobmenu hover styles, anchors and script apdate

--- src/js/mobmenu.js
+++ src/js/mobmenu.js
@@ -7,14 +7,25 @@
     // Додати атрибут data-mobmenu на бекдроп модалки
     mobmenu: document.querySelector('[data-mobmenu]'),
     closeButton: document.querySelector('[data-close-button]'),
+    anchorLinks: document.querySelectorAll('[data-mobmenu] a'),
   };
 
   refs.openMobMenu.addEventListener('click', toggleMobMenu);
   refs.closeMobMenu.addEventListener('click', toggleMobMenu);
 
+  // Додати обробник для кожного якірного посилання
+  refs.anchorLinks.forEach(link => {
+    link.addEventListener('click', closeMobMenu);
+  });
+
   function toggleMobMenu() {
     // is-open це клас який буде додаватися/забиратися на бекдроп при натисканні на кнопки
     refs.mobmenu.classList.toggle('is-open');
     refs.closeButton.classList.toggle('is-open');
+  }
+  function closeMobMenu() {
+    // Закриваємо меню, видаляючи клас is-open
+    refs.mobmenu.classList.remove('is-open');
+    refs.closeButton.classList.remove('is-open');
   }
 })();
